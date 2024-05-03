dc4636a06438f88fd8bd87f016b3f194c08b4167
caiosoaresf <caiosoaresf2005@gmail.com>
2024-05-03 12:24:41 -0300

    Add files via upload

---
+++ Python/Horseshoe calculator.py
@@ -0,0 +1,9 @@
+print("Horseshoes calculator")
+
+CV = int(float(input("Insert the number of horses: ")))
+print(CV)
+
+FR = CV * 4 
+FRT = FR + 0.15 * FR 
+
+print("The amount of horseshoes with 15 percent reserve is: ",FRT)
